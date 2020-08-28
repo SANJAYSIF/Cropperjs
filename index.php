70a09ada533880b42b2a586a9683e1e482e844a3
SNJYSIF <sanay.kumar@singleinterface.com>
2020-08-29 02:28:12 +0530

    done

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <title>Crop Image Before Upload using Cropper JS</title>
     <meta name="_token" content="">
     <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"/>
     <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <script src="js/bootstrap.min.js" ></script>
     <link rel="stylesheet" href="css/cropper.css"  />
+    <link rel="stylesheet" href="css/main.css"  />
     <script src="js/cropper.js" ></script>
 </head>
 <style type="text/css">
@@ -24,6 +26,10 @@ img {
 .modal-lg{
   max-width: 1000px !important;
 }
+.modal-open-new .modal {
+  overflow-y: auto;
+  overflow-x: hidden;
+}
 </style>
 <body>
 <div class="container">
@@ -32,44 +38,279 @@ img {
     <input type="file" name="image" class="image">
     </form>
 </div>
+<?php include 'popup.php';?>
 
-<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalLabel">PHP Crop Image Before Upload using Cropper JS - NiceSnippets.com</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="img-container">
-            <div class="row">
-                <div class="col-md-8">
-                    <img id="image" src="https://avatars0.githubusercontent.com/u/3456749">
-                </div>
-                <div class="col-md-4">
-                    <div class="preview"></div>
-                </div>
-            </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-primary" id="crop">Crop</button>
-      </div>
-    </div>
-  </div>
-</div>
 
 </div>
 </div>
 <script>
 
 var $modal = $('#modal');
+var $modalChild = $('#getCroppedCanvasModal');
 var image = document.getElementById('image');
 var cropper;
+var Cropper = window.Cropper;
+var URL = window.URL || window.webkitURL;
+var container = document.querySelector('.img-container');
+var image = container.getElementsByTagName('img').item(0);
+var download = document.getElementById('download');
+var actions = document.getElementById('actions');
+var dataX = document.getElementById('dataX');
+var dataY = document.getElementById('dataY');
+var dataHeight = document.getElementById('dataHeight');
+var dataWidth = document.getElementById('dataWidth');
+var dataRotate = document.getElementById('dataRotate');
+var dataScaleX = document.getElementById('dataScaleX');
+var dataScaleY = document.getElementById('dataScaleY');
+var options = {
+    aspectRatio: 16 / 9,
+    preview: '.img-preview',
+    ready: function (e) {
+      console.log(e.type);
+    },
+    cropstart: function (e) {
+      console.log(e.type, e.detail.action);
+    },
+    cropmove: function (e) {
+      console.log(e.type, e.detail.action);
+    },
+    cropend: function (e) {
+      console.log(e.type, e.detail.action);
+    },
+    crop: function (e) {
+      var data = e.detail;
 
+      console.log(e.type);
+      dataX.value = Math.round(data.x);
+      dataY.value = Math.round(data.y);
+      dataHeight.value = Math.round(data.height);
+      dataWidth.value = Math.round(data.width);
+      dataRotate.value = typeof data.rotate !== 'undefined' ? data.rotate : '';
+      dataScaleX.value = typeof data.scaleX !== 'undefined' ? data.scaleX : '';
+      dataScaleY.value = typeof data.scaleY !== 'undefined' ? data.scaleY : '';
+    },
+    zoom: function (e) {
+      console.log(e.type, e.detail.ratio);
+    }
+  };
+  //var cropper = new Cropper(image, options);
+  var originalImageURL = image.src;
+  var uploadedImageType = 'image/jpeg';
+  var uploadedImageName = 'cropped.jpg';
+  var uploadedImageURL;
+
+  // Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Buttons
+  if (!document.createElement('canvas').getContext) {
+    $('button[data-method="getCroppedCanvas"]').prop('disabled', true);
+  }
+
+  if (typeof document.createElement('cropper').style.transition === 'undefined') {
+    $('button[data-method="rotate"]').prop('disabled', true);
+    $('button[data-method="scale"]').prop('disabled', true);
+  }
+
+  // Download
+  if (typeof download.download === 'undefined') {
+    download.className += ' disabled';
+    download.title = 'Your browser does not support download';
+  }
+  // Options
+  actions.querySelector('.docs-toggles').onchange = function (event) {
+    var e = event || window.event;
+    var target = e.target || e.srcElement;
+    var cropBoxData;
+    var canvasData;
+    var isCheckbox;
+    var isRadio;
+
+    if (!cropper) {
+      return;
+    }
+
+    if (target.tagName.toLowerCase() === 'label') {
+      target = target.querySelector('input');
+    }
+
+    isCheckbox = target.type === 'checkbox';
+    isRadio = target.type === 'radio';
+
+    if (isCheckbox || isRadio) {
+      if (isCheckbox) {
+        options[target.name] = target.checked;
+        cropBoxData = cropper.getCropBoxData();
+        canvasData = cropper.getCanvasData();
+
+        options.ready = function () {
+          console.log('ready');
+          cropper.setCropBoxData(cropBoxData).setCanvasData(canvasData);
+        };
+      } else {
+        options[target.name] = target.value;
+        options.ready = function () {
+          console.log('ready');
+        };
+      }
+
+      // Restart
+      cropper.destroy();
+      cropper = new Cropper(image, options);
+    }
+  };
+  // Methods
+  actions.querySelector('.docs-buttons').onclick = function (event) {
+    var e = event || window.event;
+    var target = e.target || e.srcElement;
+    var cropped;
+    var result;
+    var input;
+    var data;
+
+    if (!cropper) {
+      return;
+    }
+
+    while (target !== this) {
+      if (target.getAttribute('data-method')) {
+        break;
+      }
+
+      target = target.parentNode;
+    }
+
+    if (target === this || target.disabled || target.className.indexOf('disabled') > -1) {
+      return;
+    }
+
+    data = {
+      method: target.getAttribute('data-method'),
+      target: target.getAttribute('data-target'),
+      option: target.getAttribute('data-option') || undefined,
+      secondOption: target.getAttribute('data-second-option') || undefined
+    };
+
+    cropped = cropper.cropped;
+
+    if (data.method) {
+      if (typeof data.target !== 'undefined') {
+        input = document.querySelector(data.target);
+
+        if (!target.hasAttribute('data-option') && data.target && input) {
+          try {
+            data.option = JSON.parse(input.value);
+          } catch (e) {
+            console.log(e.message);
+          }
+        }
+      }
+
+      switch (data.method) {
+        case 'rotate':
+          if (cropped && options.viewMode > 0) {
+            cropper.clear();
+          }
+
+          break;
+
+        case 'getCroppedCanvas':
+          try {
+            data.option = JSON.parse(data.option);
+          } catch (e) {
+            console.log(e.message);
+          }
+
+          if (uploadedImageType === 'image/jpeg') {
+            if (!data.option) {
+              data.option = {};
+            }
+
+            data.option.fillColor = '#fff';
+          }
+
+          break;
+      }
+
+      result = cropper[data.method](data.option, data.secondOption);
+
+      switch (data.method) {
+        case 'rotate':
+          if (cropped && options.viewMode > 0) {
+            cropper.crop();
+          }
+
+          break;
+
+        case 'scaleX':
+        case 'scaleY':
+          target.setAttribute('data-option', -data.option);
+          break;
+
+        case 'getCroppedCanvas':
+          if (result) {
+            // Bootstrap's Modal
+            $('#getCroppedCanvasModal').modal().find('.modal-body').html(result);
+
+            if (!download.disabled) {
+              download.download = uploadedImageName;
+              download.href = result.toDataURL(uploadedImageType);
+            }
+          }
+
+          break;
+
+        case 'destroy':
+          cropper = null;
+
+          if (uploadedImageURL) {
+            URL.revokeObjectURL(uploadedImageURL);
+            uploadedImageURL = '';
+            image.src = originalImageURL;
+          }
+
+          break;
+      }
+
+      if (typeof result === 'object' && result !== cropper && input) {
+        try {
+          input.value = JSON.stringify(result);
+        } catch (e) {
+          console.log(e.message);
+        }
+      }
+    }
+  };
+
+document.body.onkeydown = function (event) {
+    var e = event || window.event;
+
+    if (e.target !== this || !cropper || this.scrollTop > 300) {
+      return;
+    }
+
+    switch (e.keyCode) {
+      case 37:
+        e.preventDefault();
+        cropper.move(-1, 0);
+        break;
+
+      case 38:
+        e.preventDefault();
+        cropper.move(0, -1);
+        break;
+
+      case 39:
+        e.preventDefault();
+        cropper.move(1, 0);
+        break;
+
+      case 40:
+        e.preventDefault();
+        cropper.move(0, 1);
+        break;
+    }
+  };
 $("body").on("change", ".image", function(e){
     var files = e.target.files;
     var done = function (url) {
@@ -96,14 +337,36 @@ $("body").on("change", ".image", function(e){
 });
 
 $modal.on('shown.bs.modal', function () {
-    cropper = new Cropper(image, {
-    aspectRatio: 1,
-    viewMode: 3,
-    preview: '.preview'
-    });
+    cropper = new Cropper(image, options);
 }).on('hidden.bs.modal', function () {
    cropper.destroy();
    cropper = null;
+});
+$("body").on("change", "#inputImage", function(e){
+    var files = e.target.files;
+    var done = function (url) {
+      image.src = url;
+      cropper = new Cropper(image, options);
+    };
+    var reader;
+    var file;
+    var url;
+
+    if (files && files.length > 0) {
+      cropper.destroy();
+      file = files[0];
+
+      if (URL) {
+
+        done(URL.createObjectURL(file));
+      } else if (FileReader) {
+        reader = new FileReader();
+        reader.onload = function (e) {
+          done(reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
 });
 
 $("#crop").click(function(){
@@ -128,10 +391,17 @@ $("#crop").click(function(){
                     console.log(data);
                     $modal.modal('hide');
                     alert("success upload image");
+                },
+                error: function() {
+                  alert("There was an error. Try again please!");
                 }
               });
          }
     });
+});
+$('.canvaseModalClose').click(function(){
+    $modalChild.modal('hide')
+    $('body').addClass('modal-open-new');
 })
 
 </script>
